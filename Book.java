de68297f7fb33a06d2c8fa0d44110813e0211dab
scottlesley <smlesley@email.sc.edu>
2022-03-15 12:52:12 -0400

    Created the stubs for Book.java and BookHotel.java

---
+++ Book.java
@@ -0,0 +1,14 @@
+public abstract class Book {
+    private String Book;
+    private boolean Cancel;
+    private String date;
+    private String type;
+
+    public Book(String date, String location){
+
+    }
+
+    public void Cancel(){
+        
+    }
+}
---
+++ BookHotel.java
@@ -0,0 +1,11 @@
+public class BookHotel {
+    private String nameHotel;
+    private String location;
+    private String guest;
+    private RoomType roomType;
+    private Brand HotelBrands;
+    
+    public Book(){
+        
+    }
+}
