c1ed98db024a7a4a2f1afa66513893b86a58e55b
scottlesley <smlesley@email.sc.edu>
2022-03-17 12:03:46 -0400

    Added class stub and getInstance() method to GenerateDaysBooked.java

---
+++ GenerateDaysBooked.java
@@ -0,0 +1,9 @@
+public class GenerateDaysBooked {
+    private static GenerateDaysBooked generateDaysBooked;
+
+    private GenerateDaysBooked(){}
+
+    //TODO Implement private readJson() method
+
+    public static GenerateDaysBooked getInstance(){}
+}
