c9dc35f8995f1b57de2f92126d771dd02e4449f4
Gazy Rubied <grubied@email.sc.edu>
2022-03-28 18:36:33 -0400

    updated User Constant

---
+++ RegisteredUserConstant.java
@@ -0,0 +1,14 @@
+public class RegisteredUserConstant {
+    protected static final String USER_ID = "id";
+    protected static final String USER_FIRST_NAME= "firstName";
+    protected static final String USER_LAST_NAME="lastName";
+    protected static final String USER_DATE_OF_BIRTH="dateOfBirth";
+    protected static final String USER_BILLING_ADDRESS= "billingAddress";
+    protected static final Boolean USER_DISCOUNT="false";
+    protected static final Boolean USER_REWARD_MEMBER="false";
+    protected static final int USER_NUM_FLIGHT= 2;
+    protected static final String USER_REVIEW="review";
+    protected static final String USER_PAST_RESERVATION="pastReservation";
+    protected static final String USER_BLACKLISTED_AIRPORTS="blacklistedAirports";
+    protected static final String USER_PETS="pets";
+}
