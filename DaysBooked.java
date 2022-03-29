e43832f9868b1882a825988a94651f41ef9d7bcf
scottlesley <smlesley@email.sc.edu>
2022-03-29 12:14:33 -0400

    Make the DaysBooked constructor work better with it's generator class

--- DaysBooked.java
+++ DaysBooked.java
@@ -4,9 +4,10 @@ public class DaysBooked {
     private String id;
 
 
-    public DaysBooked(int year, int[] dayRange) {
+    public DaysBooked(int year, int[] dayRange, String id) {
         this.year = year;
         this.dayRange = dayRange;
+        this.id = id;
     }
 
     public void setYear(int year) {
